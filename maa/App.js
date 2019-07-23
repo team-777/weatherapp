75ebbb5e1a080437b8c10a2cc8412f07cb13baa2
sywtit <youwrothit17@naver.com>
2019-07-23 16:21:03 +0900

    add handler function

--- maa/App.js
+++ maa/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    <Text style = {[styles.textBig, styles.textRed]}>Hello, World!</Text>
-   <Button title={'click me!'} onPress={() => console.log('Clicked')}/>
-    </View>
-  );
+export default function App() extends React.Component{
+    clickHandler = () => console.log('Clicked!');
+
+    render(){
+      return (
+      <View style={styles.container}>
+          <Text style = {[styles.textBig, styles.textRed]}>Hello, World!</Text>
+         <Button title={'click me!'}
+          onPress={
+          this.clickHandler
+          }/>
+          </View>
+          );
+    }
 }
 
 const styles = StyleSheet.create({
