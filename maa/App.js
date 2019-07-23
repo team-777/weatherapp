460cf78a45d1a3504e1e7a6bb493a83ad69773a5
sywtit <youwrothit17@naver.com>
2019-07-23 16:08:39 +0900

    add Button click

--- maa/App.js
+++ maa/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
     <Text style = {[styles.textBig, styles.textRed]}>Hello, World!</Text>
+   <Button title={'click me!'} onPress={() => console.log('Clicked')}/>
     </View>
   );
 }
